a4e277b8c9f5f588a9eeb46b55de8baf47cc7719
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2021-11-15 16:32:46 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -21,3 +21,24 @@ menu.onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
+
+// Faqs
+const accor = document.querySelectorAll(".accordion-item-header");
+
+accor.forEach(accorItems => {
+    accorItems.addEventListener("click", event => {
+        const current = document.querySelector(".accordion-item-header.active");
+        if(current && current !== accorItems){
+             current.classList.toggle("active");
+             current.nextElementSibling.style.maxHeight = 0;
+        }
+
+        accorItems.classList.toggle("active");
+        const accorbody = accorItems.nextElementSibling;
+        if(accorItems.classList.contains("active")){
+            accorbody.style.maxHeight = accorbody.scrollHeight + "px"
+        }else{
+            accorbody.style.maxHeight = 0;
+        }
+    });
+});
