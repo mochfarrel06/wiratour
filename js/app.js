b1f3b587737bee52062e75442c03912004b823ac
tyoprataama <tyoprataama18@gmail.com>
2021-11-17 19:19:02 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,6 +10,45 @@ setInterval(function(){
     }
 }, 5000);
 
+// Show active menu when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesMenu = document.querySelector('#services-page');
+    const teamMenu = document.querySelector('#team-page');
+    let scrollPos = window.scrollY;
+    // console.log(scrollPos);
+  
+    // adds 'highlight' class to my menu items
+    if (window.innerWidth > 960 && scrollPos < 600) {
+      homeMenu.classList.add('highlight');
+      aboutMenu.classList.remove('highlight');
+      return;
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+      aboutMenu.classList.add('highlight');
+      homeMenu.classList.remove('highlight');
+      servicesMenu.classList.remove('highlight');
+      teamMenu.classList.remove('highlight');
+      return;
+    } else if (window.innerWidth > 960 && scrollPos < 2600) {
+      servicesMenu.classList.add('highlight');
+      teamMenu.classList.remove('highlight');
+      aboutMenu.classList.remove('highlight');
+      return;
+    } else if (window.innerWidth > 960 && scrollPos < 8000) {
+        teamMenu.classList.add('highlight');
+        servicesMenu.classList.remove('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;}
+  
+    if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+      elem.classList.remove('highlight');
+    }
+  };
+  window.addEventListener('scroll', highlightMenu);
+  window.addEventListener('click', highlightMenu);
+
 // Faqs
 const accor = document.querySelectorAll(".accordion-item-header");
 
