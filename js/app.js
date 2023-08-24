f74ed29e4f7c44c767fb59df5008e8fc7bfb85c9
mochfarrel06 <mochfarrel2606@gmail.com>
2023-08-25 00:12:52 +0700

    Memodifikasi halaman home

--- js/app.js
+++ js/app.js
@@ -1,40 +1,40 @@
 // Image slider
 var counter = 1;
 
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
+setInterval(function () {
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
 
-    if(counter > 4){
-        counter = 1;
-    }
+  if (counter > 4) {
+    counter = 1;
+  }
 }, 2000);
 
 // Faqs
-const accor = document.querySelectorAll(".accordion-item-header");
+const accor = document.querySelectorAll('.accordion-item-header');
 
-accor.forEach(accorItems => {
-    accorItems.addEventListener("click", event => {
-        const current = document.querySelector(".accordion-item-header.active");
-        if(current && current !== accorItems){
-             current.classList.toggle("active");
-             current.nextElementSibling.style.maxHeight = 0;
-        }
+accor.forEach((accorItems) => {
+  accorItems.addEventListener('click', (event) => {
+    const current = document.querySelector('.accordion-item-header.active');
+    if (current && current !== accorItems) {
+      current.classList.toggle('active');
+      current.nextElementSibling.style.maxHeight = 0;
+    }
 
-        accorItems.classList.toggle("active");
-        const accorbody = accorItems.nextElementSibling;
-        if(accorItems.classList.contains("active")){
-            accorbody.style.maxHeight = accorbody.scrollHeight + "px"
-        }else{
-            accorbody.style.maxHeight = 0;
-        }
-    });
+    accorItems.classList.toggle('active');
+    const accorbody = accorItems.nextElementSibling;
+    if (accorItems.classList.contains('active')) {
+      accorbody.style.maxHeight = accorbody.scrollHeight + 'px';
+    } else {
+      accorbody.style.maxHeight = 0;
+    }
+  });
 });
 
 // navbar
-const menuToggle = document.querySelector('.menu-toggler input');
+const menuToggle = document.getElementById('nav-toggle');
 const nav = document.querySelector('header .navbar');
 
-menuToggle.addEventListener('click', function(){
-    nav.classList.toggle('slide');
+menuToggle.addEventListener('click', function () {
+  nav.classList.toggle('slide');
 });
